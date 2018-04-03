6cf07fc87a9e41f7c6c44f1c3b45a4e99cc3e16c
DESKTOP-IBH3VN3\User <pctmdehd19@naver.com>
2018-04-03 19:31:02 +0900

    프로필 설정 액티비티/XML

---
+++ app/src/main/java/com/prograpy/app2/appdev2/profile/profileActivity.java
@@ -0,0 +1,15 @@
+package com.prograpy.app2.appdev2.profile;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.prograpy.app2.appdev2.R;
+
+public class profileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+}
