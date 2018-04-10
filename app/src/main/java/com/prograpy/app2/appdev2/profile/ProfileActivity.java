0d539a87cbd501084ae6725462595a810a581dbc
DESKTOP-IBH3VN3\User <pctmdehd19@naver.com>
2018-04-10 20:30:59 +0900

    프로필 닉네임 중복확인 처리

--- app/src/main/java/com/prograpy/app2/appdev2/profile/ProfileActivity.java
+++ app/src/main/java/com/prograpy/app2/appdev2/profile/ProfileActivity.java
@@ -5,18 +5,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.prograpy.app2.appdev2.R;
 import com.prograpy.app2.appdev2.main.SubActivity;
 
 public class ProfileActivity extends AppCompatActivity {
     private Button join;
+    private Button nickname;
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
 
         join = (Button)findViewById(R.id.join);
+        nickname = (Button)findViewById(R.id.nickname_btn);
+        editText = (EditText)findViewById(R.id.nameText);
 
         join.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +31,23 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        nickname.setOnClickListener(new View.OnClickListener()
+    {
+
+            public void onClick(View view)
+            {
+                final String nick = editText.getText().toString();
+                if(nick.equals(""))
+                {
+                    Toast.makeText(ProfileActivity.this, "값을 입력하세요.", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(ProfileActivity.this, "사용 가능한 닉네임입니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
     }
 }
