d1a919339b1243e03d1281132269aa4ac81e2fe1
DESKTOP-FGSM3I6\samsung <ataraxiady@gmail.com>
2018-05-08 20:28:07 +0900

    넘기기,좋아요 버튼 구현

--- app/src/main/java/com/prograpy/app2/appdev2/main/SubActivity.java
+++ app/src/main/java/com/prograpy/app2/appdev2/main/SubActivity.java
@@ -9,7 +9,9 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.prograpy.app2.appdev2.R;
@@ -28,6 +30,10 @@ public class SubActivity extends AppCompatActivity{
     private Intent intent;
     ImageButton imageButtonUser;
     ImageButton imageButtonSettings;
+    Button dislikeButton;
+    Button likeButton;
+    int index=0;
+    TextView textView;
 
     private View.OnClickListener listener = new View.OnClickListener() {
         @Override
@@ -57,6 +63,41 @@ public class SubActivity extends AppCompatActivity{
             public void onClick(View view) {
                 intent = new Intent(getApplicationContext(), SetActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        dislikeButton = (Button) findViewById(R.id.btn_dislike);
+        dislikeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                index++;
+
+                if (adapter.getCount() > index){
+                    viewPager.setCurrentItem(index);}
+                else {
+                   Toast.makeText(SubActivity.this,"오늘 소개는 끝났습니다.",Toast.LENGTH_LONG).show();
+                   viewPager.setVisibility(View.GONE);
+                   textView = (TextView)findViewById(R.id.textView);
+                   textView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        likeButton = (Button) findViewById(R.id.btn_like);
+        likeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                index++;
+
+                if (adapter.getCount() > index){
+                    viewPager.setCurrentItem(index);}
+                    //서버연동 시 추가
+                else {
+                    Toast.makeText(SubActivity.this,"오늘 소개는 끝났습니다.",Toast.LENGTH_LONG).show();
+                    viewPager.setVisibility(View.GONE);
+                    textView = (TextView)findViewById(R.id.textView);
+                    textView.setVisibility(View.VISIBLE);
+                }
+
             }
         });
 
@@ -97,7 +138,7 @@ public class SubActivity extends AppCompatActivity{
 
         @Override
         public int getCount() {
-            return 1;
+            return 5;
         }
 
     }
