845ec8c086b64ff36d352584394ea42fab3c0f43
이승준 <rbfaosldk@hubilon.com>
2018-07-02 18:00:59 +0900

    서버 api 목록을 나열하는 클래스 추가

---
+++ app/src/main/java/com/prograpy/app2/appdev2/network/response/ApiValue.java
@@ -0,0 +1,13 @@
+package com.prograpy.app2.appdev2.network.response;
+
+/**
+ * 서버에 요청할 API path 정보를 나열해놓은 클래스
+ * 필요한 path에 따라서 값을 입력하면 됨
+ */
+public class ApiValue {
+
+    // 회원가입 요청
+    public static final String API_JOIN = "/addUserInfo";
+
+
+}
