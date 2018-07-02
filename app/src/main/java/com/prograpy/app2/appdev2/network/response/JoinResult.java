01f3a3221e4daf45cfdf01aa9ff159921d37a654
이승준 <rbfaosldk@hubilon.com>
2018-07-02 18:07:08 +0900

    회원가입 레이아웃 일부 수정 / 회원가입 관련 task 요청 및 연동 추가 / 회원가입 클래스 정리

--- app/src/main/java/com/prograpy/app2/appdev2/network/response/JoinResult.java
+++ app/src/main/java/com/prograpy/app2/appdev2/network/response/JoinResult.java
@@ -2,12 +2,35 @@ package com.prograpy.app2.appdev2.network.response;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * 서버에서 전달받은 데이터를 파싱해주는 클래스
+ *
+ */
 public class JoinResult {
+
+    // 서버의 json 데이터중 어떤 키값의 데이터를 파싱시킬 건지 정하는 게
+    // @SerializedName 부분
 
 
     @SerializedName("success")
     private boolean success = false;
 
-    @SerializedName("msg")
-    private String msg = "";
+    @SerializedName("error")
+    private String error = "";
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
 }
