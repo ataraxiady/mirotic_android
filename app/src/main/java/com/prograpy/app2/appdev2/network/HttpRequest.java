b1e3095538e8f2fcbaad88a47842fc4d2d94394a
이승준 <rbfaosldk@hubilon.com>
2018-07-02 18:01:09 +0900

    서버요청 클래스 수정

--- app/src/main/java/com/prograpy/app2/appdev2/network/HttpRequest.java
+++ app/src/main/java/com/prograpy/app2/appdev2/network/HttpRequest.java
@@ -1,5 +1,7 @@
 package com.prograpy.app2.appdev2.network;
 
+import android.util.Log;
+
 import com.google.gson.Gson;
 
 import java.io.BufferedReader;
@@ -23,13 +25,39 @@ public class HttpRequest {
     private static final int TIMEOUT_CONNECTION = 30000;
     private static final int TIMEOUT_READ = 30000;
 
-    public static String callRequestServer (String serverUrl, String api_path, String method, Map<String, Object> params) throws Exception{
-        return startRequest(serverUrl, api_path, method, params);
+    private static final String SERVER_URL = "http://13.209.78.126:8080/appDev2";
+
+    /**
+     * 서버 실행 요청을 전달하는 함수
+     * @param api_path 요청할 api의 경로
+     * @param method post / get
+     * @param params 각 방식에 맞는 파라미터
+     * @return 서버에서 받은 json 결과를 string으로 리턴
+     * @throws Exception 주로 connection이 원활히 이루어지지 않았을 때 발생한다.
+     */
+    public static String callRequestServer (String api_path, String method, Map<String, Object> params) throws Exception{
+        return startRequest(api_path, method, params);
     }
 
-    public static String startRequest(String serverUrl, String api_path, String method, Map<String, Object> params) throws Exception{
+    /**
+     * 실질적으로 서버쪽에 요청하는 함수
+     * @param api_path 요청할 api의 경로
+     * @param method post / get
+     * @param params 각 방식에 맞는 파라미터
+     * @return 서버에서 받은 json 결과를 string으로 리턴
+     * @throws Exception 주로 connection이 원활히 이루어지지 않았을 때 발생한다.
+     */
+    public static String startRequest(String api_path, String method, Map<String, Object> params) throws Exception{
+        URL url = null;
 
-        URL url = createUrl(serverUrl, api_path, params);
+        if(method.equalsIgnoreCase("post")){
+            url  = createUrl(api_path, null);
+        }else{
+
+            url  = createUrl(api_path, params);
+        }
+
+        Log.d(TAG, "request url > " + url.toString());
 
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
@@ -52,6 +80,8 @@ public class HttpRequest {
                 Gson gson = new Gson();
                 String postJson = gson.toJson(params);
 
+                Log.d(TAG, "request send post json > " + postJson);
+
                 connection.setDoOutput(true);
                 connection.setRequestProperty("Content-Length", Integer.toString(postJson.getBytes("utf-8").length));
                 OutputStream os = connection.getOutputStream();
@@ -61,6 +91,7 @@ public class HttpRequest {
             }
 
         }catch (Exception e) {
+            e.printStackTrace();
             throw new Exception("API response error : " + 0);
         }
 
@@ -98,12 +129,12 @@ public class HttpRequest {
 
 
 
-    public static URL createUrl (String serverUrl, String api_path, Map<String, Object> params) throws MalformedURLException, UnsupportedEncodingException {
+    public static URL createUrl (String api_path, Map<String, Object> params) throws MalformedURLException, UnsupportedEncodingException {
 
         boolean isFirst = true;
 
         StringBuffer urlStr = new StringBuffer();
-        urlStr.append(serverUrl);
+        urlStr.append(SERVER_URL);
         urlStr.append(api_path);
 
         if(params != null && params.size() > 0){
