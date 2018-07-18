32848ea65acca027ddb194ba46003c7e0fcfaf88
이승준 <rbfaosldk@hubilon.com>
2018-07-18 11:41:32 +0900

    푸쉬 받고 메세지 받은 상대에 따라 파일 저장 다르게하는 로직 추가 / 키보드 완료 액션 리스너 추가

--- app/src/main/java/com/prograpy/app2/appdev2/chat/ChatMainActivity.java
+++ app/src/main/java/com/prograpy/app2/appdev2/chat/ChatMainActivity.java
@@ -1,18 +1,30 @@
 package com.prograpy.app2.appdev2.chat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.prograpy.app2.appdev2.LoginActivity;
 import com.prograpy.app2.appdev2.R;
 import com.prograpy.app2.appdev2.network.response.ApiValue;
 import com.prograpy.app2.appdev2.network.response.data.ChatData;
@@ -102,9 +114,25 @@ public class ChatMainActivity extends AppCompatActivity {
 
         chatMainAdapter.setChatLists(chatList);
         chatMainAdapter.notifyDataSetChanged();
+        chatListView.scrollToPosition(chatList.size() - 1);
 
         edInputMsg = (EditText) findViewById(R.id.editText);
+        edInputMsg.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_SEND ||
+                        actionId == EditorInfo.IME_ACTION_DONE){
 
+                    msg = edInputMsg.getText().toString();
+
+                    if(!msg.isEmpty())
+                        sendTask();
+
+                    edInputMsg.setText("");
+                }
+                return false;
+            }
+        });
         edInputMsg.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -136,29 +164,40 @@ public class ChatMainActivity extends AppCompatActivity {
         super.onNewIntent(intent);
 
         Log.d("onNewIntent", intent.getStringExtra("msg"));
+        Log.d("onNewIntent", intent.getStringExtra("matchId"));
 
-        if(intent.getStringExtra("msg") != null &&
-                !intent.getStringExtra("msg").isEmpty()){
+        // 메세지를 받긴 받았는데 다른 상대가 보낸 메세지일 경우는
+        // 노티를 보여주고 그 상대방 파일에 저장
+        // 아니면 현재 채팅방에서 노출
+        if(matchId.equals(intent.getStringExtra("matchId"))){
 
-            long now = System.currentTimeMillis();
-            Date date = new Date(now);
-            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            if(intent.getStringExtra("msg") != null &&
+                    !intent.getStringExtra("msg").isEmpty()){
 
-            msg = intent.getStringExtra("msg");
-            msgType = 1;
+                long now = System.currentTimeMillis();
+                Date date = new Date(now);
+                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
-            ChatData chatData = new ChatData();
-            chatData.setChatMsg(msg);
-            chatData.setChatTime(format.format(date));
-            chatData.setChatType(msgType);
-            chatList.add(chatData);
+                msg = intent.getStringExtra("msg");
+                msgType = 1;
 
-            String fileText = msg + "," + format.format(date) + "," + msgType;
-            fileUtils.writeFileText(fileName, fileText);
+                ChatData chatData = new ChatData();
+                chatData.setChatMsg(msg);
+                chatData.setChatTime(format.format(date));
+                chatData.setChatType(msgType);
+                chatList.add(chatData);
 
+                String fileText = msg + "," + format.format(date) + "," + msgType;
+                fileUtils.writeFileText(fileName, fileText);
+
+            }
+
+            chatMainAdapter.notifyDataSetChanged();
+            chatListView.scrollToPosition(chatList.size() - 1);
+        }else{
+            sendNotification(intent.getStringExtra("msg"), intent.getStringExtra("matchId"));
         }
 
-        chatMainAdapter.notifyDataSetChanged();
     }
 
     @Override
@@ -166,6 +205,7 @@ public class ChatMainActivity extends AppCompatActivity {
         super.onResume();
 
         chatMainAdapter.notifyDataSetChanged();
+        chatListView.scrollToPosition(chatList.size() - 1);
     }
 
     private void sendTask(){
@@ -192,6 +232,8 @@ public class ChatMainActivity extends AppCompatActivity {
 
 
                     chatMainAdapter.notifyDataSetChanged();
+                    chatListView.scrollToPosition(chatList.size() - 1);
+
                 }else{
                     Toast.makeText(ChatMainActivity.this, result.getMsg(), Toast.LENGTH_SHORT).show();
                 }
@@ -210,6 +252,55 @@ public class ChatMainActivity extends AppCompatActivity {
 
 
         sendMsgTask.execute(ApiValue.API_SEND_MSG, PreferenceData.getKeyUserId(), matchId, msg, format.format(date));
+    }
+
+
+    private void sendNotification(String messageBody, String matchId) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = "com.prograpy.app2.appdev2";
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.mipmap.logo2)
+                        .setContentTitle(matchId)
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            String channelName = "meyou";
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+
+
+        FileUtils fileUtils = new FileUtils(this);
+
+        String fileName = PreferenceData.getKeyUserId() + "_" + matchId + "_chat";
+
+        if(!fileUtils.isFileExists(fileName)){
+            fileUtils.createFile(fileName);
+        }
+
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+        String fileText = messageBody + "," + format.format(date) + "," + 1;
+        fileUtils.writeFileText(fileName, fileText);
+
     }
 
 }
