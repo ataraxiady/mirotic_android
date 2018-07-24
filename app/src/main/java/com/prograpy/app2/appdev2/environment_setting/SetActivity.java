0b8f9369462bc6f924b9aeb60165e75b589d5075
DESKTOP-IBH3VN3\User <pctmdehd19@naver.com>
2018-07-24 17:25:08 +0900

    고객센터(메일보내기) 추가

--- app/src/main/java/com/prograpy/app2/appdev2/environment_setting/SetActivity.java
+++ app/src/main/java/com/prograpy/app2/appdev2/environment_setting/SetActivity.java
@@ -9,20 +9,32 @@ import android.widget.TextView;
 import com.prograpy.app2.appdev2.R;
 
 public class SetActivity extends AppCompatActivity {
-    private TextView vs;
+    private TextView vs, sendMail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_setting);
         vs = findViewById(R.id.version);
+        sendMail = findViewById(R.id.send);
+
 
         vs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SetActivity.this, SettingAppVersion.class);
-                startActivity(intent);
+                Intent version = new Intent(SetActivity.this, SettingAppVersion.class);
+                startActivity(version);
             }
         });
+
+        sendMail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent send = new Intent(SetActivity.this, SendMail.class);
+            }
+        });
+
+
+
   }
 }
