d0574fd13d480b2c8f26befae95c5aecc2c47c3d
EseungJun <6951004@naver.com>
2018-07-26 22:34:57 +0900

    int to string

--- app/src/main/java/com/prograpy/app2/appdev2/main/InfoFragment.java
+++ app/src/main/java/com/prograpy/app2/appdev2/main/InfoFragment.java
@@ -52,7 +52,7 @@ public class InfoFragment extends Fragment {
 
         if (getArguments() != null) {
             fname.setText(getArguments().getString(NAME));
-            fage.setText(getArguments().getString(AGE));
+            fage.setText(String.valueOf(getArguments().getInt(AGE)));
             fgender.setText(getArguments().getString(GENDER));
             farea.setText(getArguments().getString(AREA));
 
